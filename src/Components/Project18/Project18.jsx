2d1be6c55342daad5abef88c9c3439d8d9fa761c
Umarov Rahmatillo <rahmatillo722022@gmail.com>
2024-07-08 18:31:11 +0500

    main

--- src/Components/Project18/Project18.jsx
+++ src/Components/Project18/Project18.jsx
@@ -4,7 +4,7 @@ const Project18 = () => {
   return (
     <div>
       <div className="parent">
-        <img src="https://picsum.photos/410/" alt="image" />
+        <img src="https://i.pinimg.com/236x/2a/6b/bb/2a6bbb6053df272f1d35149211ec0e1a.jpg" alt="" />
       </div>
     </div>
   );
